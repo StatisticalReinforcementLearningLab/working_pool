04d9168493fafe946139aa3cb2d1dbd7dde7adbe
Sabina321 <sabina.tomkins@gmail.com>
2019-07-20 09:03:13 -0400

    clipped

--- simulation/sim_working.py
+++ simulation/sim_working.py
@@ -296,7 +296,7 @@ def run_many(algo_type,cases,sim_start,sim_end,update_time,dist_root,write_direc
                 if algo_type=='pooling_four':
                     psi = ['location']
                 
-                glob,personal = initialize_policy_params_TS(experiment,7,standardize=False,baseline_features=baseline,psi_features=psi,responsivity_keys=responsivity_keys,algo_type =algo_type,hob_params={'degree':1.0})
+                glob,personal = initialize_policy_params_TS(experiment,u,standardize=False,baseline_features=baseline,psi_features=psi,responsivity_keys=responsivity_keys,algo_type =algo_type,hob_params={'degree':1.0})
                 
                 hist = new_kind_of_simulation(experiment,'TS',personal,glob,feat_trans=feat_trans,algo_type=algo_type,case=case,sim_num=sim,train_type=train_type)
                 to_save = make_to_save(experiment)
